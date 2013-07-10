64652b1a70993324a98551aaa0266d8857088f2f
dpuruman <dpuruman@gmail.com>
2013-07-10 04:10:39 +0000

    test hellow

--- web.js
+++ web.js
@@ -3,9 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var fs = require('fs')
-  var k = fs.readFileSync('./index.html','utf8');  
-  response.send(k);
+  var fs = require('fs');
+//  var k = fs.readFileSync('./index.html','utf8');  
+//  response.send(k);
+    response.send("Hello workd");
 });
 
 var port = process.env.PORT || 5000;
